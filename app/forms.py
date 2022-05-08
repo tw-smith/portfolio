2b31aab6db5a9acd942db030272ccdf16be6a190
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-05-08 20:47:27 +0100

    Add email contact form functionality (at last!)
    Add functionality whereby someone sending a message via the contact
    form will send me an email with their details and the message text.
    Implemented via Google workspaces.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField, EmailField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired(), Email()])
-    message = StringField('Message', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter your name"})
+    email = EmailField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Enter your email address."})
+    message = TextAreaField('Message', validators=[DataRequired()], render_kw={"placeholder": "Enter your message.", "rows": 10})
     submit = SubmitField('Send Message')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from app import app
+from flask_mail import Message
+from app import app, mail
 from app.forms import ContactForm
 
 @app.route('/', methods=['GET','POST'])
@@ -7,5 +8,11 @@ from app.forms import ContactForm
 def index():
     form = ContactForm()
     if form.validate_on_submit():
-        pass   # submit form
+        msg = Message("Portfolio Email", recipients=["toby2367@googlemail.com"])
+
+        msg.body = '''Message from: {sender}.
+                      Email address: {email}.
+                      Message:
+                      {message}'''.format(sender=form.name, email=form.email, message=form.message)
+        mail.send(msg)
     return render_template('index.html',form=form)
