5147b9c6ae297560a072c86a3e03ca837977c04d
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-05-08 21:12:57 +0100

    Change formatting of email which is sent
    The formatting of the email that was being sent from the form was
    off. This commit fixes that. Attempted to implement a flash message
    to say the message was sent sucessfully but this isn't working for
    some reason.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from flask_mail import Message
 from app import app, mail
 from app.forms import ContactForm
@@ -11,8 +11,15 @@ def index():
         msg = Message("Portfolio Email", recipients=["toby2367@googlemail.com"])
 
         msg.body = '''Message from: {sender}.
+
                       Email address: {email}.
+
                       Message:
-                      {message}'''.format(sender=form.name, email=form.email, message=form.message)
+                      {message}'''.format(sender=form.name.data, email=form.email.data, message=form.message.data)
         mail.send(msg)
+        #flash("Message sent successfully!")
+        #return render_template('index.html',form=form)
+        return redirect('/#contact')
+
+
     return render_template('index.html',form=form)
