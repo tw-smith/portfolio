8df0496b742b33eaf81d33b08d2b61175e071848
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-06-28 20:15:25 +0100

    Adds first spam honeypot implementation
    This commit adds the first attempt at a spam honeypot implementation on the contact form.
    It also adds some basic logging to capture the date, time and email address from which the
    spam message was recieved. The logging could probably be improved as at the moment it also
    captures a bunch of the wsgi type server info messages as well. Could also write a new log
    file every day or week or something.

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()], render_kw={"placeholder": "Enter your name"})
+    lastname = StringField('Last Name', render_kw={"placeholder": "Enter your last name"}) #hp
     email = EmailField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Enter your email address."})
     message = TextAreaField('Message', validators=[DataRequired()], render_kw={"placeholder": "Enter your message.", "rows": 10})
     submit = SubmitField('Send Message')
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,18 @@ from flask import render_template, flash, redirect
 from flask_mail import Message
 from app import app, mail
 from app.forms import ContactForm
+import logging
+from datetime import datetime
 
+
+logging.basicConfig(filename="logs/hp_record.log", level="WARNING")
 @app.route('/', methods=['GET','POST'])
 @app.route('/index', methods=['GET','POST'])
 def index():
+    dipped = False
     form = ContactForm()
+
+    hp_logger = logging.getLogger()
     if form.validate_on_submit():
         msg = Message("Portfolio Email", recipients=["toby2367@googlemail.com"])
 
@@ -16,7 +23,15 @@ def index():
 
                       Message:
                       {message}'''.format(sender=form.name.data, email=form.email.data, message=form.message.data)
-        mail.send(msg)
+
+        if form.lastname.data:
+            dipped = True
+        if dipped:
+            now = datetime.now()
+            hp_logger.warning(now)
+            hp_logger.warning('Spam email from: %s', form.email.data)
+        else:
+            mail.send(msg)
         #flash("Message sent successfully!")
         #return render_template('index.html',form=form)
         return redirect('/#contact')
