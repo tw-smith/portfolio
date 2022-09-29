df887b2cffd88334f7a8bb542bdde68a5fbe0377
tw-smith <92607103+tw-smith@users.noreply.github.com>
2022-09-29 22:23:09 +0100

    finalises sendgrid implementation and form flashes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import ContactForm
 from sendgrid import SendGridAPIClient
@@ -12,27 +12,31 @@ ssl._create_default_https_context = ssl._create_unverified_context
 @app.route('/index', methods=['GET','POST'])
 def index():
     form = ContactForm()
-
-    if form.validate_on_submit():
-        message = Mail(from_email = app.config['EMAIL_ADDRESS'], 
-                       to_emails = app.config['EMAIL_ADDRESS'], 
-                       subject = "Portfolio Email", 
-                       plain_text_content='''Message from: {sender}
-                       
-                                             Email address: {email}
-                                        
-                                             Message: {message}'''.format(sender=form.name.data, email=form.email.data, message=form.message.data))
-        try:
-            sg = SendGridAPIClient(app.config['SENDGRID_API_KEY'])
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(e.message)
-
-        return redirect('/#contact')
-    return render_template('index.html',form=form) #TODO flash some sort of error message here because form submission has not validated
+    if request.method == 'GET':
+        return render_template('index.html', form=form)
+    elif request.method == 'POST':
+        print('in post branch')
+        print(form.validate_on_submit())
+        print(form.errors)
+        if form.validate_on_submit():
+            message = Mail(from_email = app.config['EMAIL_ADDRESS'], 
+                        to_emails = app.config['EMAIL_ADDRESS'], 
+                        subject = "Portfolio Email", 
+                        plain_text_content='''Message from: {sender}
+                        
+                                                Email address: {email}
+                                            
+                                                Message: {message}'''.format(sender=form.name.data, email=form.email.data, message=form.message.data))
+            try:
+                sg = SendGridAPIClient(app.config['SENDGRID_API_KEY'])
+                response = sg.send(message)
+                flash("Success! I'll get back to you as soon as I can.")
+            except Exception as e:
+                flash('Error! Message was not sent.')
+            return redirect('/#contact')
+        else:
+            flash('Error! Form validation or reCAPTCHA failed.')
+            return redirect('/#contact')
 
 
 
